72661eeb5c7574dea37e43004cfea54e5e9c5181
alextraterrestre <alexis.carlier07@gmail.com>
2026-03-05 10:39:25 +0100

    feat(texttest): add unit test for min & max quality

---
+++ python/test.py
@@ -0,0 +1,18 @@
+from gilded_rose import Item, GildedRose
+import pytest
+
+def test_normal_item_degrades_quality_and_sellin():
+    item = Item("+5 Dexterity Vest", 10, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    
+    assert item.sell_in == 9
+    assert item.quality == 19
+
+def test_normal_item_degrades_quality_and_sellin():
+    item = Item("+5 Dexterity Vest", 10, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    
+    assert item.sell_in == 9
+    assert item.quality == item.quality.min(0) & item.quality.max(50)
